80a630395fc7d5a7d3cf1c8323ba994e0600f154
OrKatz7 <Or.Katz1@gmail.com>
2021-08-23 21:43:59 +0300

    Update dataset.py

--- src/02_train/dataset.py
+++ src/02_train/dataset.py
@@ -21,7 +21,7 @@ class RSNADatasetTrain(Dataset):
     
     def __getitem__(self,idx):
         data = np.load(self.paths[idx])
-        img = data['img'].astype(np.uint8)
+        img = data['img'].astype(np.uint8)[:,:,:3]
         img = cv2.resize(img, (self.w,self.h), interpolation=cv2.INTER_AREA)
         mask =  data['mask'].astype(np.uint8)
         mask = cv2.resize(mask, (self.w,self.h), interpolation=cv2.INTER_AREA)
