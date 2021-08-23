d8694529ca25a94b3212f3c4323a805ac04fd6c7
OrKatz7 <Or.Katz1@gmail.com>
2021-08-23 21:55:53 +0300

    Update train_02.py

--- src/02_train/train_02.py
+++ src/02_train/train_02.py
@@ -72,8 +72,8 @@ if __name__=='__main__':
     # train
     
     for seed in config['split_seed_list']:
-        trn_idxs_list = [glob.glob(INPUT_PATH.format("train"))][0:100]
-        val_idxs_list = [glob.glob(INPUT_PATH.format("val"))][0:100]
+        trn_idxs_list = [glob.glob(INPUT_PATH.format("train"))[0:100]]
+        val_idxs_list = [glob.glob(INPUT_PATH.format("val"))[0:100]]
 #         print(trn_idxs_list[0][0:10])
 #         print(val_idxs_list[0][0:10])
         with open(opj(config['OUTPUT_PATH'],f'trn_idxs_list_seed{seed}'), 'wb') as f:
